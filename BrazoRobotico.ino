5f48cf460dbaca454e1aa45a2f6e9f3c1bd8e426
DanielBotero <daniel.boterop@uqvirtual.edu.co>
2019-07-01 20:06:21 -0500

    Control de servos por monitor serial

--- BrazoRobotico.ino
+++ BrazoRobotico.ino
@@ -1,34 +1,48 @@
-//Este programa mueve el motor de la Base mediante el bluetooth 
 #include <Servo.h>
-#include <SoftwareSerial.h>
+Servo motorBase, motorDerecha, motorIzquierda, motorMano;
 
-int bluetoothTx = 8; // Declara el pin conectado del bluetooth
-int bluetoothRx = 11;
-#define motorBase 12 //Declaro pin de la base
-#define motorDerecha 11
-#define motorIzquierda 10
-#define motorMano 9
-Servo Base;
-Servo Izquierda;
-Servo Derecha;
-Servo Mano;  
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx); 
 
 void setup() {
-{
-  Base.attach(12); 
-  Serial.begin(9600);
-  //Configuracion de conexion del bluetooth a la app en android
-  bluetooth.begin(9600);
+Serial.begin(9600);
+motorBase.attach(12);
+motorDerecha.attach(4);
+motorIzquierda.attach(3);
+motorMano.attach(9);
 }
-}
-void loop() {
-  //Recibe la informacion en el bluetooth
-  if(bluetooth.available()> 0 ) //Recibe el grado en el bluetooth
-  {
-    int servopos = bluetooth.read(); // Guarda el grado recibido en servopos
-    Serial.println(servopos); 
-    Base.write(servopos); // gira el servo en el ángulo recibido desde la aplicación de Android
-  }
 
+char servo;
+int angulo;
+int Base,Derecha,Izquierda,Mano;
+
+void loop() {
+leer_Dato();
+switch(servo)
+{
+case 'B':
+Base = angulo;
+break;
+case 'D':
+Derecha = angulo;
+break;
+case 'I':
+Izquierda = angulo;
+break;
+case 'M':
+Mano = angulo;
+break;
+
+}
+motorBase.write(Base);
+motorDerecha.write(Derecha);
+motorIzquierda.write(Izquierda);
+motorMano.write(Mano);
+}
+
+void leer_Dato()
+{
+  if (Serial.available() > 0)
+  {
+    servo = Serial.read();
+    angulo = Serial.parseInt();
+  }
 }
