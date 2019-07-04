fb871669d85dbd90ebb1518420521dad55bdf019
DanielBotero <daniel.boterop@uqvirtual.edu.co>
2019-07-04 09:19:25 -0500

    Codigo comentado

--- BrazoRobotico.ino
+++ BrazoRobotico.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
-Servo motorBase, motorDerecha, motorIzquierda, motorMano;
-SoftwareSerial BT(2,3);
-void leer_DatoBT();
-void leer_Dato();
+Servo motorBase, motorDerecha, motorIzquierda, motorMano; //Declaracion de los nombres de los 4 servos
+SoftwareSerial BT(2,3); // pin del modulo BT
+void leer_DatoBT(); //Declaracion de funcion para administrar datos desde el modulo BT
+void leer_Dato(); //Declaracion de funcion para administrar datos desde el monitor serial  
 
 void setup() {
 Serial.begin(9600);
@@ -11,15 +11,20 @@ motorBase.attach(12);
 motorDerecha.attach(4);
 motorIzquierda.attach(5);
 motorMano.attach(9);
+//Declaracion de pines de los servos
 }
 
 char servo;
 int angulo;
 int Base,Derecha,Izquierda,Mano;
+//Declaracion de variables globales para capturar datos ingresados mediante monitor o BT
 
 void loop() {
-leer_Dato();
-leer_DatoBT();
+leer_Dato();//Definicion de funcion para administrar datos desde el modulo BT, donde servo y angulo toman un valor ingresado
+leer_DatoBT();//Definicion de funcion para administrar datos desde el monitor serial, donde servo y angulo toman un valor ingresad
+//LLamar funciones
+
+//2*
 switch(servo)
 {
 case 'B':
@@ -34,29 +39,37 @@ break;
 case 'M':
 Mano = angulo;
 break;
-
+//Asignacion de cada servo a la variable entera "angulo" con respecto al caracter inicial   
 }
+//3*
 motorBase.write(Base);
 motorDerecha.write(Derecha);
 motorIzquierda.write(Izquierda);
 motorMano.write(Mano);
+//Escrible en el servo el valor entero del angulo enviado
 }
-
+//1*
 void leer_Dato()
 {
-  if (Serial.available() > 0)
+  if (Serial.available() > 0) //Si se recibió un dato en el monitor serial 
   {
-    servo = Serial.read();
-    angulo = Serial.parseInt();
+    servo = Serial.read();//Asigna al servo el caracter ingresado
+    angulo = Serial.parseInt();//Asigna a la variable angulo el valor entero enviado
   }
 }
-
-
+//1.1*
 void leer_DatoBT()
 {
-  if (BT.available() > 0)
+ 
+  BT.println(BT.read());
+  
+  if (BT.available() > 0)//Si se recibó un dato desde el BT
   {
-    servo = BT.read();
-    angulo = BT.parseInt();
+    servo = BT.read();//Asigna al servo el caracter ingresado
+    angulo = BT.parseInt();//Asigna a la variable angulo el valor entero enviado
+    BT.println(servo);
+  }else{
+    BT.println("No se está recibiendo un Dato");
   }
+  
 }
